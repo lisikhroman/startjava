4a46a6cdc71ea0f45533464ed183a08d57344ff5
unknown <Roman.Lisikh@indusoft.ru>
2021-11-15 21:30:01 +0700

    delete dot before main method; declared variables as close as possible to where they were first used; when checking for boolean values, implicitly set the value to true

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,28 +1,28 @@
 public class ConditionalStatement {
-    public static void main (String[] args) {
-        int age = 32;
-        boolean maleGender = true;
-        float height = 2.1f;
-        char firstNameLetter = 'M';
+    public static void main(String[] args) {
 
+        int age = 32;
         if(age > 20) {
             System.out.println("Возраст " + age + " больше 20");
         }
 
-        if(maleGender == true) {
+        boolean maleGender = true;
+        if(maleGender) {
             System.out.println("Мужской пол");
         }
 
-        if(maleGender != false) {
-            System.out.println("Мужской пол");
+        if(!maleGender) {
+            System.out.println("Женский пол");
         }
 
+        float height = 2.1f;
         if(height < 1.80) {
             System.out.println("Низкий");
         } else {
             System.out.println("Высокий");
         }
 
+        char firstNameLetter = 'M';
         if(firstNameLetter == 'M') {
             System.out.println("Первая буква имени " + firstNameLetter);
         } else if(firstNameLetter == 'I') {
