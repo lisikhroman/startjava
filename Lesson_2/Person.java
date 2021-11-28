e706b89ca6e9ac9256975f0aba5d530779279e36
unknown <Roman.Lisikh@indusoft.ru>
2021-11-28 20:33:29 +0700

    implement HW_5_lesson2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    String gender = "мужской";
+    String name = "Роман";
+    float height = 178.5f;
+    float weight = 90.7f;
+    int age = 31;
+
+    void move() {
+        System.out.println("Вы идёте");
+    }
+
+    boolean sit() {
+        System.out.println("Вы сели");
+        return true;
+    }
+
+    void run() {
+        System.out.println("Вы перешли на бег");
+    }
+
+    String talk() {
+        return "Привет";
+    }
+
+    void learnJava() {
+        System.out.println("Вы изучаете Java");
+    }
+}
