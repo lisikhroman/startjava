9b9e057bbc064cd99688ecec243bfa5456324db7
unknown <Roman.Lisikh@indusoft.ru>
2021-12-05 19:35:52 +0700

    implement HW_7_lesson_2

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,28 +1,72 @@
 public class Wolf {
 
-    String gender;
-    String nickname;
-    String colour;
-    float weight;
-    int age;
+    private String gender;
+    private String nickname;
+    private String colour;
+    private float weight;
+    private int age;
 
-    void move() {
+    public String getGender() {
+        return gender;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public int getAge() {
+        return age;
+    } 
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setAge(int age) {
+        if(age > 8) {
+           System.out.println("Некорректный возраст"); 
+        } else {
+            this.age = age;
+        }
+    }
+
+    public void move() {
         System.out.println("идет");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("сидит");
     }
 
-    void run() {
+    public void run() {
         System.out.println("бежит");
     }
 
-    String howl() {
+    public String howl() {
         return "воет";
     }
 
-    String hunt() {
+    public String hunt() {
         return "охотится";
     }
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,17 +2,17 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.gender = "самец";
-        wolfOne.nickname = "Дружок";
-        wolfOne.colour = "коричневый";
-        wolfOne.weight = 15.5f;
-        wolfOne.age = 7;
-
-        System.out.println(wolfOne.gender + " волка");
-        System.out.println("по кличке " + wolfOne.nickname);
-        System.out.println("имеет " + wolfOne.colour + " окрас");
-        System.out.println("весом он " + wolfOne.weight + " килограмм");
-        System.out.println("и возрастом " + wolfOne.age + " лет");
+        wolfOne.setGender("самец");
+        wolfOne.setNickname("Дружок");
+        wolfOne.setColour("коричневый");
+        wolfOne.setWeight(15.5f);
+        wolfOne.setAge(7);
+ 
+        System.out.println(wolfOne.getGender() + " волка");
+        System.out.println("по кличке " + wolfOne.getNickname());
+        System.out.println("имеет " + wolfOne.getColour() + " окрас");
+        System.out.println("весом он " + wolfOne.getWeight() + " килограмм");
+        System.out.println("и возрастом " + wolfOne.getAge() + " лет");
         wolfOne.move();
         wolfOne.sit();
         wolfOne.run();
